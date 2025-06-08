4927ad1d2289a2df7c081f49136e543f4475d53e
PeterUngab34 <peterpaulungab00@gmail.com>
2025-06-07 12:30:01 -1200

    Add files via upload

---
+++ rentalcar/src/rentalcar/Rentalcar.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rentalcar;
+
+/**
+ *
+ * @author USER
+ */
+public class Rentalcar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        fir open = new fir();
+        open.setVisible(true);
+    }
+    
+}
